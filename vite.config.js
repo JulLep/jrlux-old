aa0833eb94071f1ec5858fa0a4a8623e2960c749
Julien Rheaume-Lepage <rknmusic01@gmail.com>
2024-08-12 11:00:42 -0400

    installation ghpages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: '/jrlux/', // Replace <REPO_NAME> with your actual repository name
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
