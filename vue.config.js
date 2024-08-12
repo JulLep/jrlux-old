087b0052fe9267c3b5953ec73765d0f9b0483111
Julien Rheaume-Lepage <rknmusic01@gmail.com>
2024-08-12 10:14:08 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/your-repository-name/'  // Replace with your GitHub repository name
+    ? '/jrlux/'  // Replace with your GitHub repository name
     : '/'
 });
